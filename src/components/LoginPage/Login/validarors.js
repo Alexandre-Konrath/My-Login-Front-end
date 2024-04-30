5bf4bb49d95fa14bddf656f747d5a4bddcf5d694
Alexandre de Lima Konrath <160286787+Alexandre-Konrath@users.noreply.github.com>
2024-04-30 17:01:50 -0300

    validation of name, email and password fields

---
+++ src/components/LoginPage/Login/validarors.js
@@ -0,0 +1,21 @@
+export function validatorName(name) {
+  // Remova espaços em branco antes e depois do nome
+  name = name.trim();
+  // Regex para validar o nome
+  let namePattern = /^[A-Z][a-z]+(\s[A-Z][a-z]+)+$/;
+  return namePattern.test(name);
+}
+
+export function validatorEmail(email) {
+  // Remova espaços em branco antes e depois do email
+  email = email.trim();
+  let emailPattern = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+  return emailPattern.test(email);
+}
+
+export function validatorPassword(password) {
+  // Remova espaços em branco antes e depois da senha
+  password = password.trim();
+  let passwordPattern = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+  return passwordPattern.test(password);
+}
