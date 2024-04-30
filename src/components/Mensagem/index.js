d59dc5fd376c9de06ade71ea4765b177c87d6cda
Alexandre de Lima Konrath <160286787+Alexandre-Konrath@users.noreply.github.com>
2024-04-30 16:59:48 -0300

    message function file

---
+++ src/components/Mensagem/index.js
@@ -0,0 +1,14 @@
+export default function Mensagem(texto, cor) {
+  let container_mensagem = document.getElementById('container_mensagem');
+  if (container_mensagem) {
+    container_mensagem.textContent = texto;
+    container_mensagem.classList.add(cor); // Adiciona a classe de cor
+    container_mensagem.classList.remove('hidden');
+
+    setTimeout(() => {
+      container_mensagem.classList.add('hidden');
+    }, 3000);
+  } else {
+    console.error("Elemento container_mensagem não encontrado no DOM.");
+  }
+}
