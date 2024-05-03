3ad2bec2e529f8abb56b6ea8b7199e8f2e497636
Alexandre de Lima Konrath <160286787+Alexandre-Konrath@users.noreply.github.com>
2024-05-03 14:31:17 -0300

    componentizando o footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+export default function Footer() {
+  return (
+    <footer>
+      <h1>&copy; Todos so direitos reservados</h1>
+      <p>Entre em contato: delimakonrath@gmail.com | (48) 9 9173-2972</p>
+    </footer>
+  )
+}
